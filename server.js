07883c6852d67709bee5f521a18a0b3ab4f676d9
Ronnie Powell <notesmane@ronnies-mac-pro.myfiosgateway.com>
2023-08-27 23:30:24 -0400

    Almost there, still bugs

--- server.js
+++ server.js
@@ -1,33 +1,55 @@
 //^ Build an express application that enables users to count down the number of bottles of beer.
 
+// sets up the page
 const express = require('express');
-
 const app = express();
 const PORT = 3000;
 
 // data
-const data = [];
+// const data = [];
+
+// ========================================
 
 // route
 app.get('/', (req, res) => {
-    // const num = 99;
-        res.send('working');
-//     res.send(`
-//     <h1>${num} Bottles of beer on the wall</h1>
-//     <a href='/98'a>Take one down, pass it around</a>`);
+    const num = 99;
+        // res.send('working');
+    res.send(`
+    <h1>${num} Bottles of beer on the wall, ${num} bottles of beer!</h1>
+    <a href='/${num - 1}'a>Take one down, pass it around</a>`);
+});
+
+app.get('/:number_of_bottles', (req, res) => {
+    const {number_of_bottles} = req.params
+    const bottleDown = number_of_bottles - 1;
+    res.send(`
+    <h1>${number_of_bottles} Bottles of beer on the wall...</h1>
+    <h1>${number_of_bottles} Bottles of beer on the wall, ${number_of_bottles} of beer!</h1>
+    <a href='/:${bottleDown}'a>Take one down, pass it around</a>`)
+});
+
+
+app.get(':/bottleDown', (req, res) => {
+    if(bottleDown > 0) {
+        res.send(`
+        <h1>${bottleDown} Bottles of beer on the wall</h1>
+        <a href=''a>Take one down, pass it around</a>
+        `);
+    } else {
+        res.send(`
+        <h1>No more bottles left. :(</h1>
+        `);
+    }
+});
+
+
+// app.get('/data', (req, res) => {
+//     res.status(data);
 // });
 
-// app.get('/98', (req, res) => {
-//     const newNum = num - 1;
-//     res.send(`
-//     <h1>Take one down, pass it around</h1>
-//     <a href=`)
-});
+// ========================================
 
-app.get('/data', (req, res) => {
-    res.status(data);
-});
-
+// listens for the port
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
